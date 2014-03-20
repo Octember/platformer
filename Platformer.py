131cec3b21b9d2dd110b2a3fe9d2fac1727a47b3
Noah Lindner <nwlindner@gmail.com>
2014-03-20 14:35:10 -0700

    lowercase name

--- Platformer.py
+++ Platformer.py
@@ -9,16 +9,16 @@ def main():
 	# Initialize
 	pygame.init()
 
-	grid, walls = load_map('map.db')
-	map_width, map_height = len(grid[0]) * BLOCK_SIZE, len(grid) * BLOCK_SIZE
+	map = Map('map.db')
 
 	# Number of screen_rows and cols visible on the screen at any given point
-	screen_cols, screen_rows = 30, 20
-	screen_width, screen_height = screen_cols * BLOCK_SIZE, screen_rows * BLOCK_SIZE
+	
+	screen_width, screen_height = 500, 500
+	screen_cols, screen_rows = screen_width / BLOCK_SIZE, screen_height / BLOCK_SIZE
 	screen = pygame.display.set_mode((screen_width, screen_height))
 	pygame.display.set_caption('Zoom Man 1.0')
 
-	player = Player()
+	player = Player(map.start_position)
 	clock = pygame.time.Clock()
 	font = pygame.font.SysFont(None, 30)
 
@@ -48,12 +48,12 @@ def main():
 			if event.type == MOUSEBUTTONDOWN:
 				pos = pygame.mouse.get_pos()
 				x, y = int(pos[0] + screen_x) / BLOCK_SIZE, int(pos[1] + screen_y) / BLOCK_SIZE
-				tile = grid[y][x]
+				tile = map.grid[y][x]
 				if event.button == 1 and tile.color == LAND:
-						walls.remove(tile.rect)
+						map.walls.remove(tile.rect)
 						tile.color = SKY
 				elif event.button == 3 and tile.color == SKY:
-					 	walls.append(tile)
+					 	map.walls.append(tile)
 					 	tile.color = LAND
 
 		elapsed_time = clock.tick()
@@ -62,7 +62,7 @@ def main():
 		# Horizontal movement
 		player.update_x(elapsed_time)
 
-		collisions = [wall for wall in walls if wall.collide(player.rect())]
+		collisions = [wall for wall in map.walls if wall.collide(player.rect())]
 		for wall in collisions:
 			player.collide_x(wall)
 
@@ -70,16 +70,16 @@ def main():
 		player.y_velocity += GRAVITY * elapsed_time
 		player.update_y(elapsed_time)
 
-		collisions = [wall for wall in walls if wall.collide(player.rect())]
+		collisions = [wall for wall in map.walls if wall.collide(player.rect())]
 		for wall in collisions:
 			player.collide_y(wall)
 
 		screen_x, screen_y = max(player.x - 250, 0), max(player.y - 250, 0)
-		screen_x, screen_y = min(screen_x, map_width - screen_width), min(screen_y, map_height - screen_height)
+		screen_x, screen_y = min(screen_x, map.width - screen_width), min(screen_y, map.height - screen_height)
 
 		# Draw the visible map
 		grid_x, grid_y = int(screen_x) / BLOCK_SIZE, int(screen_y) / BLOCK_SIZE
-		for row in grid[grid_y : grid_y + screen_rows + 1]:
+		for row in map.grid[grid_y : grid_y + screen_rows + 1]:
 			for square in row[grid_x : grid_x + screen_cols + 1]:
 				screen.fill(square.color, square.rect.move(-screen_x, -screen_y))
 
